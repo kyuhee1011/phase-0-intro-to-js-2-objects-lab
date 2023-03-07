6a36afef605af47b1293e25d6f107c7cb5acddf9
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-03-07 01:52:09 -0500

    object lab.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ray",
+    streetAddress: "4521 S Laburnum Ave, Richmond, VA 23231"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete employee[key];
+    return employee
+}
